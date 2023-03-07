e13b6c176d6b143f206a85059369314d6379eba0
Bruno-Bagarini <brunolottobagarini@gmail.com>
2023-03-06 21:05:42 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+// this program has the objective to generate mixed messages in order to make the user give some good laughs
+// this program has the ability to generate up to 1000 randow "why did the..." jokes collected from the following links:
+// https://www.rd.com/list/short-jokes/
+// https://www.beano.com/posts/why-did-jokes
+
