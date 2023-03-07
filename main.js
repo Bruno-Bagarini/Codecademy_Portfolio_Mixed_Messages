2c5c8135a81d455298f0a9575a11299a16d5036d
Bruno-Bagarini <brunolottobagarini@gmail.com>
2023-03-06 21:22:23 -0300

    creating a function to generate mixed messages and displaying it to the user

--- main.js
+++ main.js
@@ -6,3 +6,12 @@
 const subject = ['chicken', 'frog', 'yogurt', 'hipster', 'Oreo', 'M&M', 'nurse', 'math textbook', 'gym', 'bacon'];
 const action = ['cross the road', 'take the bus to work today', 'go to the art exhibition', 'burn his mouth', 'go to the dentist', 'go to school', 'need a red pen at work', 'visit the guidance counselor', 'close down', 'buy a suit'];
 const answer = ['To get to the other side', 'His car got toad away', 'Because it was cultured', 'He drank the coffee before it was cool', 'Because he lost his filling', 'It wanted to be a Smartie', 'In case she needed to draw blood', 'It needed help figuring out its problems', 'It just didnt work out', 'They wanted to look pretty crisp'];
+
+function generateMixedMessage() {
+    let randomSubject = subject[Math.floor(Math.random() * 10)];
+    let randomAction = action[Math.floor(Math.random() * 10)];
+    let randomAnswer = answer[Math.floor(Math.random() * 10)];
+    return 'Why did the ' + randomSubject + ' ' + randomAction + '? ' + randomAnswer + '!';
+}
+
+console.log(generateMixedMessage());
